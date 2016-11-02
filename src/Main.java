739f48cb866d9990d7018e596ca52033418b3f41
yongjunjung <yjjung@woowahan.com>
2016-11-03 08:27:30 +0900

    1.1.3 User 저장 / 조회 기능

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import main.java.User;
+import main.java.UserDao;
+
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+
+        User user = new User();
+        user.setId("1233");
+        user.setName("jjun");
+        user.setPassword("sss");
+
+        userDao.add(user);
+
+        System.out.println(user.getId() + "등록 성공!!");
+
+        User user2 = userDao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + "조회 성공");
+    }
+}
